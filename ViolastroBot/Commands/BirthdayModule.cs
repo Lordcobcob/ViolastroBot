6d1420d5e8f31ce88e431b78cdba7abf7e76bac9
Lordcobcob <105743637+Lordcobcob@users.noreply.github.com>
2023-09-05 22:10:50 +0100

    Update BirthdayModule.cs
    Peak mobile code which probably has issues

--- ViolastroBot/Commands/BirthdayModule.cs
+++ ViolastroBot/Commands/BirthdayModule.cs
@@ -10,24 +10,33 @@ public sealed class BirthdayModule : ModuleBase<SocketCommandContext>
 {
     [Command("bday")]
     [Discord.Commands.Summary("Assigns the birthday role to the the mentioned user.")]
-    [RequireRole(Roles.Moderator)]
-    public Task AssignBirthdayRole([Remainder] string _ = "")
-    {
-        if (Context.Message.MentionedUsers.Count == 0)
-        {
-            return Task.CompletedTask;
-        }
+    SocketUser user = userMessage.Author;
+    // Assuming you have access to the user object and the desired role name
+    string roleRestriction = "Moderator"; // Replace with the actual role name you're checking for
 
-        SocketGuildUser user = Context.Guild.GetUser(Context.Message.MentionedUsers.First().Id);
-        SocketRole birthdayRole = Context.Guild.GetRole(Roles.Birthday);
+    // Check if the user has the desired role
+    if (user.Roles.Any(role => role.Name == roleRestriction)
+    {    
+        public Task AssignBirthdayRole([Remainder] string _ = "")
+           {
+               if (Context.Message.MentionedUsers.Count == 0)
+           {
+               return Task.CompletedTask;
+           }
 
-        if (user.Roles.Any(role => role.Id == Roles.Birthday))
-        {
-            return user.RemoveRoleAsync(birthdayRole);
+           SocketGuildUser user = Context.Guild.GetUser(Context.Message.MentionedUsers.First().Id);
+           SocketRole birthdayRole = Context.Guild.GetRole(Roles.Birthday);
+
+           if (user.Roles.Any(role => role.Id == Roles.Birthday))
+           {
+               return user.RemoveRoleAsync(birthdayRole);
+           }
+        
+           user.AddRoleAsync(birthdayRole);
+        
+           return ReplyAsync($"Happy dabby birthday, {user.Mention}! Bwehehe!!");
         }
-        
-        user.AddRoleAsync(birthdayRole);
-        
-        return ReplyAsync($"Happy dabby birthday, {user.Mention}! Bwehehe!!");
     }
 }
+
+  
